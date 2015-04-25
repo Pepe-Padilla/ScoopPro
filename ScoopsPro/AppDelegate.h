0df3d4c1cb426cd562f8cacaa758b9470a5c331d
Pepe-Padilla <jpadilla.isc@gmail.com>
2015-04-25 12:37:42 +0200

    Initial Commit

---
+++ ScoopsPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScoopsPro
+//
+//  Created by Pepe Padilla on 15/25/04.
+//  Copyright (c) 2015 maxeiware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScoopsPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScoopsPro
+//
+//  Created by Pepe Padilla on 15/25/04.
+//  Copyright (c) 2015 maxeiware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
